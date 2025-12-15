44161036346a18e4ec0eb61093706c12de7b379c
grainme <marouanebouf3@gmail.com>
2025-12-15 13:59:42 +0100

    Add chirp validation endpoint with profanity filtering

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"sync/atomic"
+
+	"github.com/grainme/Chirpy/handlers"
 )
 
 const (
@@ -24,15 +26,25 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+	fmt.Fprintf(w, `
+<html>
+<body>
+<h1>Welcome, Chirpy Admin</h1>
+<p>Chirpy has been visited %d times!</p>
+</body>
+</html>`, cfg.fileserverHits.Load())
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(200)
 	w.Write([]byte("Hits reset to 0"))
+}
+
+func handler() http.Handler {
+	return http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 }
 
 func main() {
@@ -42,9 +54,10 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler()))
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlers.HandlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlers.HandlerValidateChirp)
 
 	server := &http.Server{
 		Handler: mux,
@@ -53,16 +66,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
-}
-
-func handler() http.Handler {
-	return http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	if _, err := w.Write([]byte("OK")); err != nil {
-		log.Fatal(err)
-	}
 }
